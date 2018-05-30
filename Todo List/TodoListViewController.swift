994dbee56c697af1fcdd568d0b49f572d5679fa1
liroy y <liroy.yarimi@gmail.com>
2018-05-30 10:56:09 +0300

    Add new item functionality complete.

--- Todo List/TodoListViewController.swift
+++ Todo List/TodoListViewController.swift
@@ -9,20 +9,23 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+// we don't need to declare a delegate like the group chat project because it's UITableViewController
 
-    let itemArray = ["buy milk","buy Eggos", "Destory Demagorgon"]
+    var itemArray = ["buy milk","buy Eggos", "Destory Demagorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    //MARK - Tableview Datasource Methods
+    //MARK: - Tableview Datasource Methods
     
+    // number of row in our table view
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
 
+    //print row by row in our table view
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
@@ -32,8 +35,9 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
-    //MARK - Tableview Delegate Methods
+    //MARK: - Tableview Delegate Methods
     
+    //this func call automatic when user pressed on cell in our table view
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
@@ -44,12 +48,35 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
-        
-        
-        
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //MARK: - Add New Item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todo List Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add item button on our UIAlert
+            print("Success!")
+            
+            //self.itemArray.append(textField.text ?? "new item")
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData() //refresh the table view
+        }
+        
+        alert.addTextField { (alertTextField) in //this closure call when the pop up message display
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
